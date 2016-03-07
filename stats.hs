c246fdc0442ad0a65230eacccdba21f76b4e5276
Richard Fung <richard@extrahop.com>
2016-03-07 13:44:09 -0800

    Updated stats.hs and added results

--- stats.hs
+++ stats.hs
@@ -1,5 +1,5 @@
 import Control.Applicative ((<$>), (<*>))
-import Data.List (elemIndices, isPrefixOf, stripPrefix)
+import Data.List (elemIndices, isPrefixOf, sortBy, stripPrefix)
 import Data.Maybe
 import System.Environment
 import System.IO
@@ -10,37 +10,54 @@ smallPrefix = "small_t"
 bigPrefix = "big_t"
 
 main = do
-    filenames <- getArgs
-    print filenames
-    putStrLn $ getHeader $ filenames !! 0
+    paths <- getArgs
+    let f :: FilePath -> (String, Int)
+        f x = (getPrefix $ getFileName x, getThreads $ getFileName x)
+        sortedPaths = sortBy (\a -> \b -> compare (f a) (f b)) paths
+    mapM printStats sortedPaths
 
 average :: [Double] -> Double
 average nums = (sum nums) / (fromIntegral $ length nums)
 
 getHeader :: FilePath -> String
-getHeader filePath
-    | isPrefixOf simplePrefix filename =
-        makeString "simple" $ getThreads "simple_t"
-    | isPrefixOf complexPrefix filename =
-        makeString "complex" $ getThreads "complex_t"
-    | isPrefixOf smallPrefix filename =
-        makeString "small" $ getThreads "small_t"
-    | isPrefixOf bigPrefix filename =
-        makeString "big" $ getThreads "big_t"
+getHeader filePath = makeString (getQueryType filename) $ getThreads filename
     where makeString query threads =
-              "Query: " ++ query ++ "; Threads: " ++ threads
-          getThreads s = fromJust $ stripPrefix s filename
-          removeDir s = let indices = elemIndices '/' s
+              "Query: " ++ query ++ "; Threads: " ++ (show threads)
+          filename = getFileName filePath
+          getQueryType filename
+              | getPrefix filename == simplePrefix = "simple"
+              | getPrefix filename == complexPrefix = "complex"
+              | getPrefix filename == smallPrefix = "small"
+              | getPrefix filename == bigPrefix = "big"
+
+getFileName :: FilePath -> FilePath
+getFileName filePath = removeDir filePath
+    where removeDir s = let indices = elemIndices '/' s
                             i = if length indices == 0 then -1
                                 else last indices
                         in drop (i+1) s
-          filename = removeDir filePath
 
 getNums :: Handle -> IO [Double]
 getNums handle = do eof <- hIsEOF handle
                     if eof then return []
                     else (:) <$> (read <$> hGetLine handle) <*>  getNums handle
-                            -- (num:) <$> getNums handle
+
+getPrefix :: FilePath -> String
+getPrefix fileName = head $ filter (\s -> isPrefixOf s fileName)
+                                   [simplePrefix, complexPrefix, smallPrefix
+                                   , bigPrefix]
+
+getThreads :: FilePath -> Int
+getThreads filename = read $ fromJust $ stripPrefix (getPrefix filename)
+                                                    filename
+
+printStats :: FilePath -> IO ()
+printStats filePath = do
+    putStrLn $ getHeader filePath
+    withFile filePath ReadMode $ \handle -> do
+        nums <- getNums handle
+        putStrLn $ "\taverage: " ++ (show $ average nums)
+        putStrLn $ "\tstandard deviation: " ++ (show $ stdDev nums)
 
 stdDev :: [Double] -> Double
 stdDev nums = sqrt $! variance nums
@@ -50,11 +67,3 @@ variance nums =
     let avg = average nums
         deviationsSquared = map (**2) $! map (\x -> x - avg) nums
     in average deviationsSquared
-
-printStats :: FilePath -> IO ()
-printStats filePath = do
-    putStrLn $ getHeader filePath
-    withFile filePath ReadMode $ \handle -> do
-        nums <- getNums handle
-        putStrLn $ "\taverage: " ++ (show $ average nums)
-        putStrLn $ "\tstandard deviation: " ++ (show $ stdDev nums)
